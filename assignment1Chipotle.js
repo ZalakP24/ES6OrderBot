fae2fd55e60059531d9a6ff44804a0ab1389bca1
Zalak Patel <zalakpatel2405@gmail.com>
2021-02-06 17:48:52 -0500

    Assignment Update

--- assignment1Chipotle.js
+++ assignment1Chipotle.js
@@ -5,7 +5,7 @@ const OrderState = Object.freeze({
     SIZE:   Symbol("size"),
     TOPPINGS:   Symbol("toppings"),
     SAUCE: Symbol("sauce"),
-    DRINKS:  Symbol("drinks")
+    SALAD:  Symbol("salad")
 });
 
 module.exports = class ChipotleOrder extends Order{
@@ -15,20 +15,31 @@ module.exports = class ChipotleOrder extends Order{
         this.sSize = "";
         this.sToppings = "";
         this.sSauce = "";
-        this.sDrinks = "";
+        this.sSalad = "";
         this.sItem = "Chipotle";
+        this.sPrice = 10;
     }
     handleInput(sInput){
         let aReturn = [];
+       
         switch(this.stateCur){
             case OrderState.WELCOMING:
-                this.stateCur = OrderState.SIZE;
-                aReturn.push("Welcome to Zalak's Chipotle");
+                this.stateCur = OrderState.SIZE
+                aReturn.push("Welcome to Rick's Chipotle");
                 aReturn.push("What size would you like?");
                 break;
             case OrderState.SIZE:
                 this.stateCur = OrderState.TOPPINGS
                 this.sSize = sInput;
+                if(this.sSize == "small"){
+                    this.sPrice += 2;
+                }
+                if(this.sSize == "medium"){
+                    this.sPrice += 5;
+                }
+                if(this.sSize == "large"){
+                    this.sPrice += 7;
+                }
                 aReturn.push("What toppings would you like?");
                 break;           
             case OrderState.TOPPINGS:
@@ -37,20 +48,23 @@ module.exports = class ChipotleOrder extends Order{
                 aReturn.push("Which Sauce would you like?");
                 break;
             case OrderState.SAUCE:
-                this.stateCur = OrderState.DRINKS
+                this.stateCur = OrderState.SALAD
                 this.sSauce = sInput;
-                aReturn.push("Would you like drinks with that?");
+                aReturn.push("Would you like Salad with that?");
                 break;
-            case OrderState.DRINKS:
+            case OrderState.SALAD:
                 this.isDone(true);
-                if(sInput.toLowerCase != "no"){
-                    this.sDrinks = sInput;
+                if(sInput.toLowerCase() != "no"){
+                    this.sSalad = sInput;
+                    this.sPrice += 10;
+                    
                 }
                 aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sToppings} and ${this.sSauce} Sauce`);
-                if(this.sDrinks){
-                    aReturn.push(this.sDrinks);
+                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sToppings} and ${this.sSauce} Sauce `); 
+                if(this.sSalad){
+                    aReturn.push(`and Salad`);
                 }
+                aReturn.push(`Your Total Amount is ${this.sPrice}$`)
                 let d = new Date(); 
                 d.setMinutes(d.getMinutes() + 20);
                 aReturn.push(`Please pick it up at ${d.toTimeString()}`);
